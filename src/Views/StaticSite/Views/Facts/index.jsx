f62feb16f3482140f9018d431f790508ad24e8c6
srivastavshrey <shreysrivastav21@gmail.com>
2022-05-11 16:20:52 +0530

    facts-fix

--- src/Views/StaticSite/Views/Facts/index.jsx
+++ src/Views/StaticSite/Views/Facts/index.jsx
@@ -7,8 +7,6 @@ import Images from '../../Components/Facts/imageRepo'
 import CommonBannerNavPrimary from '../../Components/CommonBannerNavPrimary'
 import FactsDecadeText from '../../Components/Facts/FactsDecadeText'
 
-
-
 const OurFacts = () => {
   const [decade, setDecade] = useState(0)
   const imageObjMap = {
@@ -23,18 +21,16 @@ const OurFacts = () => {
     2: 'text2010',
     3: 'text2020',
   }
- 
+
   return (
     <>
       <CommonBannerNavPrimary />
-      <div className="ourfacts-container global-padding flex">
-        <div className="ourfacts-header">
-          <div className="ourfacts-header-text">
-            <Heading
-              smallText={'The Yoga Institute Facts'}
-            />
+      <div className='ourfacts-container global-padding flex'>
+        <div className='ourfacts-header'>
+          <div className='ourfacts-header-text'>
+            <Heading smallText={'The Yoga Institute Facts'} />
             <TimePeriodSelector onDecadeChange={setDecade} />
-            <div className="our-facts-text ">
+            <div className='our-facts-text '>
               {/* <p>
                 {`Uncover both ancient and new interesting facts about your very own,
             'The Yoga Institute'. Discover fascinating facts about Shri. Yogendra
@@ -42,23 +38,49 @@ const OurFacts = () => {
             others.`}
               </p> */}
               {FactsDecadeText[textObjMap[decade]].map((h2, idtxt) => {
-                if(idtxt===0){return (
-                  <div key={idtxt} className='decade-year'>
-                    <span>{h2}</span>
-                  </div>
-                )}
-                else{
-                  return (
-                    <p key={idtxt} className='decade-para'>{h2}</p>
-                  )
-                }
-                
+                return (
+                  <>
+                    <div key={idtxt} className='decade-year'>
+                      {decade === 0 && (
+                        <span>
+                          1990
+                          <br />-<br />
+                          2000
+                        </span>
+                      )}
+                      {decade === 1 && (
+                        <span>
+                          2000
+                          <br />-<br />
+                          2010
+                        </span>
+                      )}
+                      {decade === 2 && (
+                        <span>
+                          2010
+                          <br />-<br />
+                          2020
+                        </span>
+                      )}
+                      {decade === 3 && (
+                        <span>
+                          2020
+                          <br />-<br />
+                          Onwards
+                        </span>
+                      )}
+                    </div>
+                    <p key={idtxt} className='decade-para'>
+                      {h2}
+                    </p>
+                  </>
+                )
               })}
             </div>
           </div>
         </div>
-        <div className="ourfacts-gallery global-padding flex">
-          <div className="ourfacts-gallery-grid">
+        <div className='ourfacts-gallery global-padding flex'>
+          <div className='ourfacts-gallery-grid'>
             {Images[imageObjMap[decade]].map((img, idx) => (
               <img key={idx} src={img} className={'grid-img-' + (idx + 1)} />
             ))}
@@ -67,7 +89,7 @@ const OurFacts = () => {
             <input type={'range'} min={0} max={100} value={0} />
           </div> */}
         </div>
-        <div className="divider">{divider}</div>
+        <div className='divider'>{divider}</div>
       </div>
     </>
   )
