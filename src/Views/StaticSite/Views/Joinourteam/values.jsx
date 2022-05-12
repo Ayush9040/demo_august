951d050fa96030b90903a7e764341d07c39eb77d
shrey0114 <shrey@nexgsolution.com>
2022-05-12 10:45:57 +0530

    fxes

--- src/Views/StaticSite/Views/Joinourteam/values.jsx
+++ src/Views/StaticSite/Views/Joinourteam/values.jsx
@@ -2,43 +2,38 @@ import React from 'react'
 import CommonBanner from '../../Components/Common-banner'
 // import values from '../../assets/images/values.png'
 import './styles.scss'
-import baseDomain,{ volunteerAssets } from '../../assets/images/imageAsset'
+import baseDomain, { volunteerAssets } from '../../assets/images/imageAsset'
 
 const Values = () => {
   return (
     <>
-      <div className="Benefits-container">
+      <div className='Benefits-container'>
         <CommonBanner
           isLeftContent={false}
           Logo={false}
           Navigation={true}
-          PageType="Values"
-          // BgImage={values}
-          Heading="Values"
+          PageType='Values'
+          Heading='Values'
           isOnlyBanner={false}
           innerNav={true}
         />
       </div>
-      <div className="content-container pd-career">
-        <div className="text-content">
-          <div className="banner-heading">
+      <div className='content-container pd-career'>
+        <div className='text-content'>
+          <div className='banner-heading'>
             <h1>Title</h1>
-            <div className="bottom-line"></div>
+            <div className='bottom-line'></div>
           </div>
           <p>
-            {`Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.`}
+            {`Life is not measured by how much you have attained for yourself; but rather by how much you have given to others. - Dr. Hansaji.J.Yogendra
+              Volunteer for social causes or for the Institute's internal activities by selflessly sharing your most valuable resources: Time and Care. 
+              Volunteering activities at The Yoga Institute are based on this concept of giving to uplift the poor and underprivileged in society. These programmes function only on the concept of selflessness and kindness. Along with helping the needy, volunteers engage in a variety of additional activities that assist to keep the institute running smoothly and promote the benefits of yoga at all times. Volunteers dedicate their time to activities like teaching, admin work, kitchen work, and much more. 
+              Explore the numerous social initiatives and institutional activities carried out and choose to volunteer for the ones in which you believe you can make the most difference. Volunteers who are assured they can devote time and energy to serving others are welcome to participate in the Institute's volunteer programs.
+           `}
           </p>
         </div>
-        <div className="image-content">
-          <img src={baseDomain+volunteerAssets.valuesAssets} />
+        <div className='image-content'>
+          <img src={baseDomain + volunteerAssets.valuesAssets} />
         </div>
       </div>
     </>
