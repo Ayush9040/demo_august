e9ba910c1e1e86920698552801e0b14a53d07537
shrey0114 <shrey@nexgsolution.com>
2022-05-13 11:34:03 +0530

    volunteer-json

---
+++ src/Views/StaticSite/utils/volunteerData.js
@@ -0,0 +1,32 @@
+import baseDomain,{ socialResponsibilityAssets } from "../assets/images/imageAsset"
+export const volunteerData = [
+  {
+    id:'vp_1',
+    name:'Annam Brahma',
+    image:`${baseDomain}${socialResponsibilityAssets.annamBrahma}`,
+    description:`Over 1 million free meals have been served to the needy and poor through the Institute's most widespread social initiative Annam Brahma. Volunteers help in distributing the wholesome nutritious meals at the places where it's needed the most, like slums, municipal and government hospitals, etc. Anyone can volunteer to be a part of this selfless service and help provide healthy and free sattvik food to the needy. A long-term commitment of at least 3-months would be appreciated.`,
+    reqirements:['req1','req2','req3','req4'],
+    gallery:['add images in array'],
+    faq:[{
+      ques:`How can I register to be a part of The Yoga Institutes's Volunteering Family ?`,
+      ans:'Kindly fill in your details in the above column and we will get back to you.'
+    },
+    {
+      ques:'Can I select the timing according to my availability ?',
+      ans:'Yes, we recommend that you choose the days and times based on your availability so that you may devote your time truly.'
+    },
+    {
+      ques:`I have not undertaken any of the institute's courses. Can I volunteer ?`,
+      ans:`Yes, you can look through the many volunteer programs and choose the one that is open to everyone and doesn't require any prerequisites.`
+    },
+    {
+      ques:'If I choose to volunteer, do I have to commit for a specific time ?',
+      ans:'No, you can volunteer as per your availability. However, the institute values a long-term commitment of at least three months.'
+    },
+    {
+      ques:'Is food provided to volunteers during the program ?',
+      ans:`The institute offers free sattvik meals to volunteers who attend an entire day's program.`
+    }
+  ]
+  }
+]
