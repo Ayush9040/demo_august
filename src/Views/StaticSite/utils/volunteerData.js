c9ee3abdaa9e42dfc5d49a974a5e6b4be1a28826
dnikam1704 <dhirajnikam@nexgsolution.com>
2022-05-13 12:26:58 +0530

    volunteer-json-data

--- src/Views/StaticSite/utils/volunteerData.js
+++ src/Views/StaticSite/utils/volunteerData.js
@@ -1,32 +1,244 @@
-import baseDomain,{ socialResponsibilityAssets } from "../assets/images/imageAsset"
+import baseDomain, {
+  socialResponsibilityAssets,
+} from '../assets/images/imageAsset'
 export const volunteerData = [
   {
-    id:'vp_1',
-    name:'Annam Brahma',
-    image:`${baseDomain}${socialResponsibilityAssets.annamBrahma}`,
-    description:`Over 1 million free meals have been served to the needy and poor through the Institute's most widespread social initiative Annam Brahma. Volunteers help in distributing the wholesome nutritious meals at the places where it's needed the most, like slums, municipal and government hospitals, etc. Anyone can volunteer to be a part of this selfless service and help provide healthy and free sattvik food to the needy. A long-term commitment of at least 3-months would be appreciated.`,
-    reqirements:['req1','req2','req3','req4'],
-    gallery:['add images in array'],
-    faq:[{
-      ques:`How can I register to be a part of The Yoga Institutes's Volunteering Family ?`,
-      ans:'Kindly fill in your details in the above column and we will get back to you.'
-    },
-    {
-      ques:'Can I select the timing according to my availability ?',
-      ans:'Yes, we recommend that you choose the days and times based on your availability so that you may devote your time truly.'
-    },
-    {
-      ques:`I have not undertaken any of the institute's courses. Can I volunteer ?`,
-      ans:`Yes, you can look through the many volunteer programs and choose the one that is open to everyone and doesn't require any prerequisites.`
-    },
-    {
-      ques:'If I choose to volunteer, do I have to commit for a specific time ?',
-      ans:'No, you can volunteer as per your availability. However, the institute values a long-term commitment of at least three months.'
-    },
-    {
-      ques:'Is food provided to volunteers during the program ?',
-      ans:`The institute offers free sattvik meals to volunteers who attend an entire day's program.`
-    }
-  ]
-  }
+    id: 'vp_1',
+    name: 'Annam Brahma',
+    image: `${baseDomain}${socialResponsibilityAssets.annamBrahma}`,
+    description:
+      'Over 1 million free meals have been served to the needy and poor through the Institute\'s most widespread social initiative Annam Brahma. Volunteers help in distributing the wholesome nutritious meals at the places where it\'s needed the most, like slums, municipal and government hospitals, etc. Anyone can volunteer to be a part of this selfless service and help provide healthy and free sattvik food to the needy. A long-term commitment of at least 3-months would be appreciated.',
+    reqirements: ['req1', 'req2', 'req3', 'req4'],
+    gallery: [
+      {
+        img1: `${baseDomain}${socialResponsibilityAssets.annamBrahma}`,
+        img2: `${baseDomain}${socialResponsibilityAssets.bmcSchool}`,
+        img3: `${baseDomain}${socialResponsibilityAssets.police}`,
+        img4: `${baseDomain}${socialResponsibilityAssets.trafficPolice}`,
+        img5: `${baseDomain}${socialResponsibilityAssets.truckAsana}`,
+        img6: `${baseDomain}${socialResponsibilityAssets.palnting}`,
+      },
+    ],
+    faq: [
+      {
+        ques: 'How can I register to be a part of The Yoga Institutes\'s Volunteering Family ?',
+        ans: 'Kindly fill in your details in the above column and we will get back to you.',
+      },
+      {
+        ques: 'Can I select the timing according to my availability ?',
+        ans: 'Yes, we recommend that you choose the days and times based on your availability so that you may devote your time truly.',
+      },
+      {
+        ques: 'I have not undertaken any of the institute\'s courses. Can I volunteer ?',
+        ans: 'Yes, you can look through the many volunteer programs and choose the one that is open to everyone and doesn\'t require any prerequisites.',
+      },
+      {
+        ques: 'If I choose to volunteer, do I have to commit for a specific time ?',
+        ans: 'No, you can volunteer as per your availability. However, the institute values a long-term commitment of at least three months.',
+      },
+      {
+        ques: 'Is food provided to volunteers during the program ?',
+        ans: 'The institute offers free sattvik meals to volunteers who attend an entire day\'s program.',
+      },
+    ],
+  },
+  {
+    id: 'vp_2',
+    name: 'Teacher',
+    image: `${baseDomain}${socialResponsibilityAssets.annamBrahma}`,
+    description: '\'The Yoga Institute actively works to bring the benefits of yoga into every home around the world. Our teachers selflessly contribute to this effort by conducting classes to spread the Institute\'s values and wisdom or hosting several camps for the specially-abled, the elderly, and many others. Students who have completed a course from the institute and desire to share their learnings can volunteer to teach yoga at the institute or its branches across India to spread the gift of mind-body wellness.\'',
+    reqirements: ['req1', 'req2', 'req3', 'req4'],
+    gallery: [
+      {
+        img1: `${baseDomain}${socialResponsibilityAssets.annamBrahma}`,
+        img2: `${baseDomain}${socialResponsibilityAssets.bmcSchool}`,
+        img3: `${baseDomain}${socialResponsibilityAssets.police}`,
+        img4: `${baseDomain}${socialResponsibilityAssets.trafficPolice}`,
+        img5: `${baseDomain}${socialResponsibilityAssets.truckAsana}`,
+        img6: `${baseDomain}${socialResponsibilityAssets.palnting}`,
+      },
+    ],
+    faq: [
+      {
+        ques: 'How can I register to be a part of The Yoga Institutes\'s Volunteering Family ?',
+        ans: 'Kindly fill in your details in the above column and we will get back to you.',
+      },
+      {
+        ques: 'Can I select the timing according to my availability ?',
+        ans: 'Yes, we recommend that you choose the days and times based on your availability so that you may devote your time truly.',
+      },
+      {
+        ques: 'I have not undertaken any of the institute\'s courses. Can I volunteer ?',
+        ans: 'Yes, you can look through the many volunteer programs and choose the one that is open to everyone and doesn\'t require any prerequisites.',
+      },
+      {
+        ques: 'If I choose to volunteer, do I have to commit for a specific time ?',
+        ans: 'No, you can volunteer as per your availability. However, the institute values a long-term commitment of at least three months.',
+      },
+      {
+        ques: 'Is food provided to volunteers during the program ?',
+        ans: 'The institute offers free sattvik meals to volunteers who attend an entire day\'s program.',
+      },
+    ],
+  },
+  {
+    id: 'vp_3',
+    name: 'Course Coordinator',
+    image: `${baseDomain}${socialResponsibilityAssets.annamBrahma}`,
+    description:
+      'Throughout the year, the Institute offers a variety of educational and wellness courses, and our volunteers work diligently to ensure that these programs operate smoothly. Organizing class timing, reserving the class in advance, checking for equipment availability, coordinating with teachers, and so on are some of the responsibilities. Course coordinators are the backbone of each course and play a major role. So, if you believe you have the ability to multitask and ensure that the course runs smoothly, this could be the perfect option for you to volunteer. ',
+    reqirements: ['req1', 'req2', 'req3', 'req4'],
+    gallery: [
+      {
+        img1: `${baseDomain}${socialResponsibilityAssets.annamBrahma}`,
+        img2: `${baseDomain}${socialResponsibilityAssets.bmcSchool}`,
+        img3: `${baseDomain}${socialResponsibilityAssets.police}`,
+        img4: `${baseDomain}${socialResponsibilityAssets.trafficPolice}`,
+        img5: `${baseDomain}${socialResponsibilityAssets.truckAsana}`,
+        img6: `${baseDomain}${socialResponsibilityAssets.palnting}`,
+      },
+    ],
+    faq: [
+      {
+        ques: 'How can I register to be a part of The Yoga Institutes\'s Volunteering Family ?',
+        ans: 'Kindly fill in your details in the above column and we will get back to you.',
+      },
+      {
+        ques: 'Can I select the timing according to my availability ?',
+        ans: 'Yes, we recommend that you choose the days and times based on your availability so that you may devote your time truly.',
+      },
+      {
+        ques: 'I have not undertaken any of the institute\'s courses. Can I volunteer ?',
+        ans: 'Yes, you can look through the many volunteer programs and choose the one that is open to everyone and doesn\'t require any prerequisites.',
+      },
+      {
+        ques: 'If I choose to volunteer, do I have to commit for a specific time ?',
+        ans: 'No, you can volunteer as per your availability. However, the institute values a long-term commitment of at least three months.',
+      },
+      {
+        ques: 'Is food provided to volunteers during the program ?',
+        ans: 'The institute offers free sattvik meals to volunteers who attend an entire day\'s program.',
+      },
+    ],
+  },
+  {
+    id: 'vp_4',
+    name: 'Kitchen Work',
+    image: `${baseDomain}${socialResponsibilityAssets.annamBrahma}`,
+    description:
+      'Every day, the Institute feeds all students*, staff, teachers, volunteers, and others with healthy and nutritious sattvik meals. This enormous amount of work requires as many hands as possible to serve the meal, chop the vegetables, clean the kitchen, and so on. Our volunteers work in the kitchen with the primary purpose of selflessly serving one and all. If you believe you can best contribute to the kitchen work, you can volunteer for this program.',
+    reqirements: ['req1', 'req2', 'req3', 'req4'],
+    gallery: [
+      {
+        img1: `${baseDomain}${socialResponsibilityAssets.annamBrahma}`,
+        img2: `${baseDomain}${socialResponsibilityAssets.bmcSchool}`,
+        img3: `${baseDomain}${socialResponsibilityAssets.police}`,
+        img4: `${baseDomain}${socialResponsibilityAssets.trafficPolice}`,
+        img5: `${baseDomain}${socialResponsibilityAssets.truckAsana}`,
+        img6: `${baseDomain}${socialResponsibilityAssets.palnting}`,
+      },
+    ],
+    faq: [
+      {
+        ques: 'How can I register to be a part of The Yoga Institutes\'s Volunteering Family ?',
+        ans: 'Kindly fill in your details in the above column and we will get back to you.',
+      },
+      {
+        ques: 'Can I select the timing according to my availability ?',
+        ans: 'Yes, we recommend that you choose the days and times based on your availability so that you may devote your time truly.',
+      },
+      {
+        ques: 'I have not undertaken any of the institute\'s courses. Can I volunteer ?',
+        ans: 'Yes, you can look through the many volunteer programs and choose the one that is open to everyone and doesn\'t require any prerequisites.',
+      },
+      {
+        ques: 'If I choose to volunteer, do I have to commit for a specific time ?',
+        ans: 'No, you can volunteer as per your availability. However, the institute values a long-term commitment of at least three months.',
+      },
+      {
+        ques: 'Is food provided to volunteers during the program ?',
+        ans: 'The institute offers free sattvik meals to volunteers who attend an entire day\'s program.',
+      },
+    ],
+  },
+  {
+    id: 'vp_5',
+    name: 'Day-to-Day Activities',
+    image: `${baseDomain}${socialResponsibilityAssets.annamBrahma}`,
+    description:
+      'Extensive daily activities take place in order to keep the worlds oldest organized institute running smoothly so that it can serve everyone in goodwill each and every day. Discover the numerous day-to-day activities through which you can contribute your time and energy with a selfless attitude to help the Institute serve all without a pause.',
+    reqirements: ['req1', 'req2', 'req3', 'req4'],
+    gallery: [
+      {
+        img1: `${baseDomain}${socialResponsibilityAssets.annamBrahma}`,
+        img2: `${baseDomain}${socialResponsibilityAssets.bmcSchool}`,
+        img3: `${baseDomain}${socialResponsibilityAssets.police}`,
+        img4: `${baseDomain}${socialResponsibilityAssets.trafficPolice}`,
+        img5: `${baseDomain}${socialResponsibilityAssets.truckAsana}`,
+        img6: `${baseDomain}${socialResponsibilityAssets.palnting}`,
+      },
+    ],
+    faq: [
+      {
+        ques: 'How can I register to be a part of The Yoga Institutes\'s Volunteering Family ?',
+        ans: 'Kindly fill in your details in the above column and we will get back to you.',
+      },
+      {
+        ques: 'Can I select the timing according to my availability ?',
+        ans: 'Yes, we recommend that you choose the days and times based on your availability so that you may devote your time truly.',
+      },
+      {
+        ques: 'I have not undertaken any of the institute\'s courses. Can I volunteer ?',
+        ans: 'Yes, you can look through the many volunteer programs and choose the one that is open to everyone and doesn\'t require any prerequisites.',
+      },
+      {
+        ques: 'If I choose to volunteer, do I have to commit for a specific time ?',
+        ans: 'No, you can volunteer as per your availability. However, the institute values a long-term commitment of at least three months.',
+      },
+      {
+        ques: 'Is food provided to volunteers during the program ?',
+        ans: 'The institute offers free sattvik meals to volunteers who attend an entire day\'s program.',
+      },
+    ],
+  },
+  {
+    id: 'vp_6',
+    name: 'Expertise based ',
+    image: `${baseDomain}${socialResponsibilityAssets.annamBrahma}`,
+    description:
+      'Share your skills with the world. If you have the expertise in content writing, graphic designing, document management, computer repairs, information technology, or any other area, you can volunteer to assist the Institute in running efficiently and giving selfless service to all without interruption. Explore and be a part of the Institute’s expertise-based volunteer program; you will not only contribute to helping others but work on refining your skills. ',
+    reqirements: ['req1', 'req2', 'req3', 'req4'],
+    gallery: [
+      {
+        img1: `${baseDomain}${socialResponsibilityAssets.annamBrahma}`,
+        img2: `${baseDomain}${socialResponsibilityAssets.bmcSchool}`,
+        img3: `${baseDomain}${socialResponsibilityAssets.police}`,
+        img4: `${baseDomain}${socialResponsibilityAssets.trafficPolice}`,
+        img5: `${baseDomain}${socialResponsibilityAssets.truckAsana}`,
+        img6: `${baseDomain}${socialResponsibilityAssets.palnting}`,
+      },
+    ],
+    faq: [
+      {
+        ques: 'How can I register to be a part of The Yoga Institutess Volunteering Family?',
+        ans: 'Kindly fill in your details in the above column and we will get back to you.',
+      },
+      {
+        ques: 'Can I select the timing according to my availability ?',
+        ans: 'Yes, we recommend that you choose the days and times based on your availability so that you may devote your time truly.',
+      },
+      {
+        ques: 'I have not undertaken any of the institute\'s courses. Can I volunteer ?',
+        ans: 'Yes, you can look through the many volunteer programs and choose the one that is open to everyone and doesn\'t require any prerequisites.',
+      },
+      {
+        ques: 'If I choose to volunteer, do I have to commit for a specific time ?',
+        ans: 'No, you can volunteer as per your availability. However, the institute values a long-term commitment of at least three months.',
+      },
+      {
+        ques: 'Is food provided to volunteers during the program ?',
+        ans: 'The institute offers free sattvik meals to volunteers who attend an entire day\'s program.',
+      },
+    ],
+  },
 ]
