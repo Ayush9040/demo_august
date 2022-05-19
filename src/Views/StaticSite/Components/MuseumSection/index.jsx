75d91ec674dcb4b2fc56c167bca8353991f4f9ea
sahilrananexG <sahilrana@nexgsolution.com>
2022-05-19 11:00:15 +0530

    museum image fixed

--- src/Views/StaticSite/Components/MuseumSection/index.jsx
+++ src/Views/StaticSite/Components/MuseumSection/index.jsx
@@ -9,6 +9,7 @@ import CommonGrid from '../GalleryGrid'
 import Location from './Location'
 // import { Link } from 'react-router-dom'
 import CommonBtn from '../commonbtn'
+import baseDomain, { homeAssets, museumAssets } from '../../assets/images/imageAsset'
 const Museum = () => {
   const intro =
     'The world’s first yoga museum, Shri Yogendra Museum of Classical Yoga, defines classical yoga and its ancient and rich history. Inaugurated in 1987, by Shri Giani Zail Singh, then President of India, it presents a historical and balanced view of Yoga, and takes you through a journey of it. The museum has an array of indoor and outdoor displays, containing the History of Yoga, sections on Raja Yoga (with special reference to Patanjali Yoga Sutra), Bhakti Yoga, Mantra Yoga, Tantra Yoga, Kriyas, ancient versus modern structure of Asanas (specially elaborated – through a long and patient study of the Asana structure of The Yoga Institute), Founder Shri Yogendra Ji’s historic personal effects and useful yogic tools.'
@@ -37,9 +38,7 @@ const Museum = () => {
           return (
             <div key={i} className='museum-card-wrapper'>
               <div className='museum-card'>
-                <div className='museum-card-image'></div>
-                <div className='museum-card-image'></div>
-                <div className='museum-card-image'></div>
+                <img src={baseDomain+museumAssets.virualAssets} alt="" />
               </div>
               <p className='vt_text' >
                 Want to see what the Shri Yogendra Museum looks like from the
