c2a50dad1d8fde085b2a810f508c675b4e490e94
shrey0114 <shrey@nexgsolution.com>
2022-05-19 12:08:47 +0530

    museum-img-remove

--- src/Views/StaticSite/Components/MuseumSection/index.jsx
+++ src/Views/StaticSite/Components/MuseumSection/index.jsx
@@ -9,7 +9,10 @@ import CommonGrid from '../GalleryGrid'
 import Location from './Location'
 // import { Link } from 'react-router-dom'
 import CommonBtn from '../commonbtn'
-import baseDomain, { homeAssets, museumAssets } from '../../assets/images/imageAsset'
+import baseDomain, {
+  homeAssets,
+  museumAssets,
+} from '../../assets/images/imageAsset'
 const Museum = () => {
   const intro =
     'The world’s first yoga museum, Shri Yogendra Museum of Classical Yoga, defines classical yoga and its ancient and rich history. Inaugurated in 1987, by Shri Giani Zail Singh, then President of India, it presents a historical and balanced view of Yoga, and takes you through a journey of it. The museum has an array of indoor and outdoor displays, containing the History of Yoga, sections on Raja Yoga (with special reference to Patanjali Yoga Sutra), Bhakti Yoga, Mantra Yoga, Tantra Yoga, Kriyas, ancient versus modern structure of Asanas (specially elaborated – through a long and patient study of the Asana structure of The Yoga Institute), Founder Shri Yogendra Ji’s historic personal effects and useful yogic tools.'
@@ -34,22 +37,18 @@ const Museum = () => {
         <br />
       </div>
       <div className='museum-container'>
-        {numberofSlides.map((item, i) => {
-          return (
-            <div key={i} className='museum-card-wrapper'>
-              <div className='museum-card'>
-                <img src={baseDomain+museumAssets.virualAssets} alt="" />
-              </div>
-              <p className='vt_text' >
-                Want to see what the Shri Yogendra Museum looks like from the
-                inside? Here’s your chance to take on a virtual tour;
-              </p>
-              <a href='https://artsandculture.google.com/story/QgVhfXAZr-CxIA'>
-                <CommonBtn text={'Take a virtual tour'} />
-              </a>
-            </div>
-          )
-        })}
+        <div className='museum-card-wrapper'>
+          <div className='museum-card'>
+            <img src={baseDomain + museumAssets.virualAssets} alt='' />
+          </div>
+        </div>
+        <p className='vt_text'>
+          Want to see what the Shri Yogendra Museum looks like from the inside?
+          Here’s your chance to take on a virtual tour;
+        </p>
+        <a href='https://artsandculture.google.com/story/QgVhfXAZr-CxIA'>
+          <CommonBtn text={'Take a virtual tour'} />
+        </a>
       </div>
       <CommonGrid />
       <Location />
