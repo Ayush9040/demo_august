bf8b8b3c89d8df63efb4dcddcf47c060ce366223
srivastavshrey <shreysrivastav21@gmail.com>
2022-05-25 10:51:41 +0530

    issues-fix

--- src/Views/StaticSite/Components/Activity/Activitycard.jsx
+++ src/Views/StaticSite/Components/Activity/Activitycard.jsx
@@ -11,7 +11,7 @@ const Activitycard = ({ title, description, images }) => {
         <div className="card-content-activity">
           <h1>{title}</h1>
           <br />
-          <h4 style={textTitle ? { fontSize: '2rem' } : {}}>{description}</h4>
+          <h4 style={{ fontSize: '2rem' }}>{description}</h4>
           <h3 className="explore" style={{ fontSize: '2rem' }}>
             Explore in detail &#62;&#62;
           </h3>
