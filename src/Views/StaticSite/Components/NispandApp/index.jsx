226a9b443a8419150f812df2715c91b288d88c9b
srivastavshrey <shreysrivastav21@gmail.com>
2022-05-25 17:49:08 +0530

    nispnd-redirect-added

--- src/Views/StaticSite/Components/NispandApp/index.jsx
+++ src/Views/StaticSite/Components/NispandApp/index.jsx
@@ -2,24 +2,25 @@ import React from 'react'
 import CommonBtn from '../commonbtn'
 import './style.scss'
 import baseDomain, { homeAssets } from '../../assets/images/imageAsset'
+import { Link } from 'react-router-dom'
 
 const Nispand = () => {
   return (
-    <div className="nispand-app-container global-padding">
-      <div className="nispand-content">
-        <div className="heading-container">
-          <div className="heading-content">
-            <div className="heading-logo">
+    <div className='nispand-app-container global-padding'>
+      <div className='nispand-content'>
+        <div className='heading-container'>
+          <div className='heading-content'>
+            <div className='heading-logo'>
               <img src={baseDomain + homeAssets.homeAsset25} />
             </div>
-            <div className="heading-text">
+            <div className='heading-text'>
               <h2>The Yoga Institute</h2>
               <h1>Nispand</h1>
             </div>
           </div>
-          <div className="custom-border global-top-margin"></div>
+          <div className='custom-border global-top-margin'></div>
         </div>
-        <div className="app-description">
+        <div className='app-description'>
           <p>
             Nispand, created with knowledge of over 100 years, is the premium
             meditation and sleep app with the world’s largest collection of
@@ -28,24 +29,23 @@ const Nispand = () => {
             monitor.
           </p>
         </div>
-        <CommonBtn text={'Explore More'} />
-        <div className="download-options">
+        <a href='https://nispand.com/'>
+          <CommonBtn text={'Explore More'} />
+        </a>
+        <div className='download-options'>
           <h4>GET IT NOW!</h4>
-          <div className="google-app-store">
-            <div className="google">
-              <img src={baseDomain+homeAssets.homeAsset22} />
+          <div className='google-app-store'>
+            <div className='google'>
+              <img src={baseDomain + homeAssets.homeAsset22} />
             </div>
-            <div className="apple">
-              <img
-                width={'93%'}
-                src={baseDomain+homeAssets.homeAsset23}
-              />
+            <div className='apple'>
+              <img width={'93%'} src={baseDomain + homeAssets.homeAsset23} />
             </div>
           </div>
         </div>
       </div>
-      <div className="app-preview">
-        <img src={baseDomain+homeAssets.homeAsset24} />
+      <div className='app-preview'>
+        <img src={baseDomain + homeAssets.homeAsset24} />
       </div>
     </div>
   )
