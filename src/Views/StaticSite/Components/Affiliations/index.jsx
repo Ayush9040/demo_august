bbae423c633854273b159616005c9ddc44592d12
AakashNexGen <aakashjethani@nexgsolution.com>
2022-05-27 10:05:41 +0530

    Affiliations Changes

--- src/Views/StaticSite/Components/Affiliations/index.jsx
+++ src/Views/StaticSite/Components/Affiliations/index.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import CommonBanner from '../Common-banner'
 import Heading from '../Heading'
 import './style.scss'
-import baseDomain, { giftingAssets, background } from '../../assets/images/imageAsset'
+import baseDomain, {
+  giftingAssets,
+  background,
+} from '../../assets/images/imageAsset'
 //import AffiliationImg from '../../assets/images/why_tyi.png'
 const Affiliations = () => {
-  let desc= 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, seddiam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.'
+  let desc =
+    'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, seddiam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.'
   return (
     <>
       <div className="affiliation-container">
@@ -25,11 +29,8 @@ const Affiliations = () => {
         <div className="affiliation-text">
           <Heading largeText={'The Indian Yoga Association'} />
           <p>
-            The Indian Yoga Association is a self-regulatory body of leading
-            yoga organizations of India. It is committed to promotion and
-            advancement of Yoga and its applications around the world and
-            industry-cum-self-regulatory body to facilitate activities of member
-            institutions.
+            The Indian Yoga Association is committed to promotion and
+            advancement of Yoga and its applications around the world.
           </p>
         </div>
         <div className="affiliation-logos">
@@ -43,8 +44,7 @@ const Affiliations = () => {
         <div className="affiliation-text">
           <Heading largeText={'Yoga Certification Board'} />
           <p>
-            YCB (Yoga Certification Board) established by Ministry of AYUSH,
-            Government of India, is the Board which has been set up by
+            Yoga Certification Board is the Board which has been set up by
             Government for certification in the field of Yoga.
           </p>
         </div>
@@ -53,11 +53,8 @@ const Affiliations = () => {
         <div className="affiliation-text">
           <Heading largeText={'Yoga Alliance'} />
           <p>
-            Yoga Alliance is the largest nonprofit association representing the
-            yoga community, with over 7,000 Registered Yoga Schools. It is
-            involved with advocating for self-regulation in the yoga indus- try
-            and universal access to high quality, safe, accessible, and eq-
-            uitable yoga practices.
+            Yoga Alliance is involved with advocating for self- regulation in
+            the yoga industry.
           </p>
         </div>
         <div className="affiliation-logos">
@@ -70,14 +67,13 @@ const Affiliations = () => {
         </div>
         <div className="affiliation-text">
           <Heading
-          
             smallText={'The International Association'}
             largeText={'of Yoga Therapists'}
           />
           <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-            volutpat. Ut wisi
+            IAYT supports research and education in yoga and serves as a
+            professional organization for yoga teachers and yoga therapists
+            worldwide.
           </p>
         </div>
       </div>
