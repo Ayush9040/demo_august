b9bf0f9b5cb4df7f79c229f32e8441ebf5adedb7
sakshigupta2897 <sakshig@nexgsolution.com>
2022-06-01 13:24:57 +0530

    vedio section done

--- src/Views/StaticSite/Components/MediaVideo/data.js
+++ src/Views/StaticSite/Components/MediaVideo/data.js
@@ -1,32 +1,32 @@
 export const data = [
   {
     id: 1,
-    title:'Introduction Heading to Holistic Approach',
-    link: 'https://youtu.be/embed/7sDY4m8KNLc',
+    title:'Powerful Space for Self-Transformation',
+    link: 'https://www.youtube.com/embed/jwDhDbeShQw',
     views:2000,
     thumbnail:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png',
   },
   {
     id: 2,
-    link: 'https://youtu.be/embed/YQ1vN_91KO0',
-    title:'Introduction Heading to Holistic Approach',
+    title:'The Power Of Smile',
+    link: 'https://www.youtube.com/embed/agzzDVn0i9A',
     views:2000,
     thumbnail:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png',
   },
   {
     id: 3,
-    title:'Introduction Heading to Holistic Approach',
-    link: 'https://youtu.be/embed/YQ1vN_91KO0',
+    title:'Best Pranayama for Better Sleep at Night',
+    link: 'https://www.youtube.com/embed/fOcJCXZwW-I',
     views:2000,
     thumbnail:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png',
   },
   {
     id: 4,
-    title:'Introduction Heading to Holistic Approach',
-    link: 'https://youtu.be/embed/YQ1vN_91KO0',
+    title:'4 Ways to Take Care of Hair',
+    link: 'https://www.youtube.com/embed/G1GAlC7lPcI',
     views:2000,
     thumbnail:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png',
@@ -34,7 +34,7 @@ export const data = [
   // {
   //   id: 5,
   //   title:'Introduction Heading to Holistic Approach',
-  //   link: 'https://youtu.be/embed/YQ1vN_91KO0',
+  //   link: 'https://www.youtube.com/embed/5I76YW9nW7Y',
   //   views:2000,
   //   thumbnail:
   //     'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png',
--- src/Views/StaticSite/Components/MediaVideo/index.jsx
+++ src/Views/StaticSite/Components/MediaVideo/index.jsx
@@ -1,41 +1,48 @@
 import React, { useState } from 'react'
 import { data } from './data'
 import './style.scss'
+import MediaNav from '../MediaNav'
 
 const MediaVideo = () => {
   const [videoURL, setvideoURL] = useState(
-    'https://www.youtube.com/embed/tgbNymZ7vqY'
+    'https://www.youtube.com/embed/jwDhDbeShQw'
   )
-  console.log(videoURL,'url')
+  console.log(videoURL, 'url')
   return (
-    <div className="main-div">
-      <div className="Featured">Featured Video</div>
-      <div className="all-videos">
-        <div className="play-setion">
-          <iframe
-            className="iframe-video"
-            src={videoURL}
-          ></iframe>
-          <div className="Intro">Introduction Heading to Holistic Approach</div>
-          <h5>200k views</h5>
-        </div>
+    <>
+      <div className="main-div">
+        <MediaNav title="Gallery" />
+        <div className="Featured">Featured Video</div>
+        <div className="all-videos">
+          <div className="play-setion">
+            <iframe className="iframe-video" src={videoURL}></iframe>
+            <div className="Intro">
+              Introduction Heading to Holistic Approach
+            </div>
+            <h5>200k views</h5>
+          </div>
 
-        <div className="right-video">
-          {data.map((point, idx) => {
-            console.log(idx)
-            return (
-              <div className="more-videos"onClick={() => setvideoURL(point.link)} key={point.id}>
-                <img src={point.thumbnail} />
-                <div>
-                  <h4>{point.title}</h4>
-                  <h6>{point.views}</h6>
+          <div className="right-video">
+            {data.map((point, idx) => {
+              console.log(idx)
+              return (
+                <div
+                  className="more-videos"
+                  onClick={() => setvideoURL(point.link)}
+                  key={point.id}
+                >
+                  <img className="right_side_vedio" src={point.thumbnail} />
+                  <div>
+                    <h4>{point.title}</h4>
+                    <h6>{point.views}</h6>
+                  </div>
                 </div>
-              </div>
-            )
-          })}
+              )
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
