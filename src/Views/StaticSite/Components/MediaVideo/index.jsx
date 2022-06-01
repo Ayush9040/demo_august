03fd9cbef19d018181a7b54d8971f242fda96a24
shrey0114 <shrey@nexgsolution.com>
2022-06-01 09:56:15 +0530

    issues-fix

--- src/Views/StaticSite/Components/MediaVideo/index.jsx
+++ src/Views/StaticSite/Components/MediaVideo/index.jsx
@@ -6,7 +6,7 @@ const MediaVideo = () => {
   const [videoURL, setvideoURL] = useState(
     'https://www.youtube.com/embed/tgbNymZ7vqY'
   )
-  console.log(data)
+  console.log(videoURL,'url')
   return (
     <div className="main-div">
       <div className="Featured">Featured Video</div>
@@ -14,7 +14,7 @@ const MediaVideo = () => {
         <div className="play-setion">
           <iframe
             className="iframe-video"
-            src="https://www.youtube.com/embed/tgbNymZ7vqY"
+            src={videoURL}
           ></iframe>
           <div className="Intro">Introduction Heading to Holistic Approach</div>
           <h5>200k views</h5>
@@ -24,9 +24,8 @@ const MediaVideo = () => {
           {data.map((point, idx) => {
             console.log(idx)
             return (
-              <div className="more-videos" key={point.id}>
+              <div className="more-videos"onClick={() => setvideoURL(point.link)} key={point.id}>
                 <img src={point.thumbnail} />
-                <div onClick={() => setvideoURL(point.link)}></div>
                 <div>
                   <h4>{point.title}</h4>
                   <h6>{point.views}</h6>
--- src/Views/StaticSite/Views/Home/index.jsx
+++ src/Views/StaticSite/Views/Home/index.jsx
@@ -12,6 +12,7 @@ import NewsLetter from '../../Components/NewsLetter'
 import Footer from '../../Components/Footer'
 import Blog from '../../Components/Blogs'
 import { useLocation } from 'react-router-dom'
+import MediaVideo from '../../Components/MediaVideo'
 
 const Home = () => {
   const route = useLocation()
@@ -37,7 +38,7 @@ const Home = () => {
       <VideosSection />
       <NewsLetter />
       <Footer />
-      
+      <MediaVideo/>
     </>
   )
 }
