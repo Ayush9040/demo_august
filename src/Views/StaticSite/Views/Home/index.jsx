819089d82d56e7288f1a049870fbaeb1d1d26c3d
sahilrananexG <sahilrana@nexgsolution.com>
2022-06-06 14:29:03 +0530

    media-fixed

--- src/Views/StaticSite/Views/Home/index.jsx
+++ src/Views/StaticSite/Views/Home/index.jsx
@@ -12,7 +12,6 @@ import NewsLetter from '../../Components/NewsLetter'
 import Footer from '../../Components/Footer'
 import Blog from '../../Components/Blogs'
 import { useLocation } from 'react-router-dom'
-import MediaVideo from '../../Components/MediaVideo'
 
 const Home = () => {
   const route = useLocation()
