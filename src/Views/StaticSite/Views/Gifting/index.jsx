792be5bc54e53a78c21ec9821a4fc9a9f256dac3
sakshigupta2897 <sakshig@nexgsolution.com>
2022-06-14 10:44:58 +0530

    gifting mapping done

--- src/Views/StaticSite/Views/Gifting/index.jsx
+++ src/Views/StaticSite/Views/Gifting/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import CommonBanner from '../../Components/Common-banner'
 import './style.scss'
 import SectionComponent from '../../Components/SectionComponent/index'
@@ -7,10 +7,12 @@ import SectionComponent from '../../Components/SectionComponent/index'
 import { useLocation } from 'react-router-dom'
 import baseDomain, {
   background,
-  giftingAssets,
+  //giftingAssets,
 } from '../../assets/images/imageAsset'
+import GiftingData from './data'
 
 const Gifting = () => {
+  const [gifting, setGifitng] = useState(GiftingData)
   const route = useLocation()
   console.log(route)
   useEffect(() => {
@@ -49,42 +51,18 @@ const Gifting = () => {
           bannerImg={`${baseDomain}${background.gifting}`}
           overlay="#61829DD4"
         />
-        <SectionComponent
-          page="Gifting"
-          image={`${baseDomain}${giftingAssets.careerAssets}`}
-          title="Gift Career"
-          description={
-            'Gift a Career is a special and brilliant way to assist someone to become self-reliant. Gifting your loved ones or someone in need of a direction, any of the Institute    transformational teacher training courses will provide them with a guiding light toward a successful and sufficient earning career path. Explore the extensive range of basic, intermediate, and advanced courses available to assist someone in becoming a certified yoga instructor. '
-          }
-          sectionId="gift-career"
-        />
-        <SectionComponent
-          page="Gifting"
-          image={`${baseDomain}${giftingAssets.healthAssets}`}
-          title="Gift Health & Wellness"
-          description={
-            'The best gift we can ever give is the gift of health. Health is not simply the absence of disease; it is more profoundly the presence of well-being, balance, and contentment. The Institutes signature 7-Day Health Camp or 21-Day Better Living Course is a wonderful gift to nurture the health and wellness of our loved ones. Sign up your parents, grandparents, friends, relatives, or coworkers to assist them in overcoming any physical discomforts, mental stress, or emotional tension and enjoy a Healthy & Happy Life.'
-          }
-          sectionId="Gift-health&wellness"
-        />
-        <SectionComponent
-          page="Gifting"
-          image={`${baseDomain}${giftingAssets.widsomassets}`}
-          title="Gift Wisdom"
-          description={
-            'They say you can’t buy wisdom. They’re right! But you can surely give the gift of wisdom. Browse through our extensive collection of books. Some of our titles are Yoga Sutras of Patanjali, Recipes of Happiness, Sattvik Cooking, Yoga for Back and Joint Disorders, and many more. Our publications offer exhaustive options that range from vedic philosophy, principles of yoga to modern fusion recipes with a healthy twist. Choose now!'
-          }
-          sectionId="gift-wisdom"
-        />
-        <SectionComponent
-          page="Gifting"
-          image={`${baseDomain}${giftingAssets.nispandAssets}`}
-          title="Gift Nispand"
-          description={
-            'Give someone you love the gift of peace, calm and utter bliss with Nispand. Nispand has the worlds largest collection of more than 1000 guided meditation sessions. These help you with stress management, insomnia, relationships, emotions, work-productivity, lifestyle diseases and a host of modern lifestyle challenges. Gift Nispand subscription and grant your family member, friend or colleague to experience precious moments of mental peace amidst their hectic lives.'
-          }
-          sectionId="gift-nispand"
-        />
+        {gifting.map((item) => {
+          return (
+            <>
+              <SectionComponent
+                image={item.image}
+                title={item.title}
+                description={item.description}
+                sectionId={item.key}
+              />
+            </>
+          )
+        })}
       </div>
     </>
   )
