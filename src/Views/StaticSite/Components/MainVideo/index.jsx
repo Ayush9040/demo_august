be92af238c0c5f1af6cb67a2df0257bc407a5dd9
shrey0114 <shrey@nexgsolution.com>
2022-07-06 11:44:17 +0530

    mute-unmute-issue-fix

--- src/Views/StaticSite/Components/MainVideo/index.jsx
+++ src/Views/StaticSite/Components/MainVideo/index.jsx
@@ -7,6 +7,7 @@ import { pause, play, unmute, mute } from '../../assets/icons/icon'
 const MainVideo = () => {
 
   const videoRef = useRef(null)
+  const videoRef1 = useRef(null)
   const [playing,setPlaying]=useState(true)
 
   const [isMute,setMute]=useState(true)
@@ -18,17 +19,52 @@ const MainVideo = () => {
     }else{
       videoRef.current.pause()
       setPlaying(false)
-    }
-  }
-  const muteVideo = ()=>{
-    if(videoRef.current.muted){
-      videoRef.current.muted=false
-      setMute(false)
-    }else{
       videoRef.current.muted=true
       setMute(true)
     }
   }
+  const muteVideo = ()=>{
+    if( videoRef.current.paused===true){
+      videoRef.current.muted=true
+      setMute(true)
+    }else{
+      if(videoRef.current.muted){
+        videoRef.current.muted=false
+        setMute(false)
+      }else{
+        videoRef.current.muted=true
+        setMute(true)
+      }
+    }
+  }
+
+  const playVideo1 = ()=>{
+    if(videoRef1.current.paused){
+      videoRef1.current.play()
+      setPlaying(true)
+      
+    }else{
+      videoRef1.current.pause()
+      setPlaying(false)
+      videoRef1.current.muted=true
+      setMute(true)
+    }
+  }
+  const muteVideo1 = ()=>{
+    if( videoRef1.current.paused===true){
+      videoRef1.current.muted=true
+      setMute(true)
+    }else{
+      if(videoRef1.current.muted){
+        videoRef1.current.muted=false
+        setMute(false)
+      }else{
+        videoRef1.current.muted=true
+        setMute(true)
+      }
+    }
+  }
+
 
   return (
     <>
@@ -42,18 +78,18 @@ const MainVideo = () => {
           nofullscreen 
           loop
           autoPlay={true}
-          muted={true}
+          muted={isMute}
           id='video-desk'
         // fullscreen
         >
           Your browser does not support the video tag.
         </video>
-        <button id='play-pause' onClick={ muteVideo } >{ isMute ?  mute : unmute  }</button>
+        <button id='play-pause' onClick={  muteVideo } >{ isMute ?  mute : unmute  }</button>
         <button id='mute-unmute' onClick={ playVideo } >{ playing ?  pause : play  }</button>
       </div>
       <div className='mobile-pop-video' >
         <video
-          ref={videoRef}
+          ref={videoRef1}
           src="http://ecom-static-site.oss-ap-south-1.aliyuncs.com/Home/PlayVideo/tyi-home-bg-mobile.mp4"
           controls={false}
           disablePictureInPicture
@@ -61,12 +97,12 @@ const MainVideo = () => {
           nofullscreen 
           loop
           autoPlay={true}
-          muted={true}
+          muted={isMute}
         >
           Your browser does not support the video tag.
         </video>
-        <button id='play-pause' onClick={ muteVideo } >{ isMute ?  mute : unmute  }</button>
-        <button id='mute-unmute' onClick={ playVideo } >{ playing ?  pause : play  }</button>
+        <button id='play-pause' onClick={ muteVideo1 } >{ isMute ?  mute : unmute  }</button>
+        <button id='mute-unmute' onClick={ playVideo1 } >{ playing ?  pause : play  }</button>
       </div>
     </>
   )
