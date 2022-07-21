1f2dce4ff276db6116d0258e4b36ca8b0e97c9f2
shrey0114 <shrey@nexgsolution.com>
2022-07-21 16:11:18 +0530

    modal-open

--- src/Views/StaticSite/Views/Media/Views/Print-Digital/print-media.jsx
+++ src/Views/StaticSite/Views/Media/Views/Print-Digital/print-media.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import CommanModal from '../../../../Components/CommanModal'
 import InnerNavComponent from '../../../../Components/InnerNavComponent'
 import './style.scss'
 
 const PrintMedia = () => {
+
+ 
+  const [ openModal,setOpenModal ]=useState(false)
+  const [imageUrl,setImageUrl]=useState('')
 
   const MediaNews = {
     title: 'news',
@@ -28,50 +33,53 @@ const PrintMedia = () => {
   }
 
   return (
-    <div className="news-container">
-      {/* <MediaNav title={'News'} /> */}
-      <InnerNavComponent abc={MediaNews} />
-      {/* <MediaSection
+    <>
+      <div className="news-container">
+        {/* <MediaNav title={'News'} /> */}
+        <InnerNavComponent abc={MediaNews} />
+        {/* <MediaSection
       description={description}
       title={'News'}
       subHeading={'Highligths'}
       sectionColor={'#D58173'}
     /> */}
-      <div className='news-card-container' >
-        <div className='news-card' >
-          <img src='https://www.w3schools.com/w3css/img_lights.jpg' />
-          <div className='news-details' >
-            <p>Title</p>
-            <h3>Date</h3>
+        <div className='news-card-container' >
+          <div className='news-card' onClick={()=>{setOpenModal(true);setImageUrl('https://www.w3schools.com/w3css/img_lights.jpg')}} >
+            <img src='https://www.w3schools.com/w3css/img_lights.jpg' />
+            <div className='news-details' >
+              <p>Title</p>
+              <h3>Date</h3>
+            </div>
           </div>
-        </div>
-        <div className='news-card' >
-          <img src='https://www.w3schools.com/w3css/img_lights.jpg' />
-          <div className='news-details' >
-            <p>Title</p>
-            <h3>Date</h3>
-          </div>
-        </div>        <div className='news-card' >
-          <img src='https://www.w3schools.com/w3css/img_lights.jpg' />
-          <div className='news-details' >
-            <p>Title</p>
-            <h3>Date</h3>
-          </div>
-        </div>        <div className='news-card' >
-          <img src='https://www.w3schools.com/w3css/img_lights.jpg' />
-          <div className='news-details' >
-            <p>Title</p>
-            <h3>Date</h3>
-          </div>
-        </div>        <div className='news-card' >
-          <img src='https://www.w3schools.com/w3css/img_lights.jpg' />
-          <div className='news-details' >
-            <p>Title</p>
-            <h3>Date</h3>
+          <div className='news-card' >
+            <img src='https://www.w3schools.com/w3css/img_lights.jpg' />
+            <div className='news-details' >
+              <p>Title</p>
+              <h3>Date</h3>
+            </div>
+          </div>        <div className='news-card' >
+            <img src='https://www.w3schools.com/w3css/img_lights.jpg' />
+            <div className='news-details' >
+              <p>Title</p>
+              <h3>Date</h3>
+            </div>
+          </div>        <div className='news-card' >
+            <img src='https://www.w3schools.com/w3css/img_lights.jpg' />
+            <div className='news-details' >
+              <p>Title</p>
+              <h3>Date</h3>
+            </div>
+          </div>        <div className='news-card' >
+            <img src='https://www.w3schools.com/w3css/img_lights.jpg' />
+            <div className='news-details' >
+              <p>Title</p>
+              <h3>Date</h3>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {openModal && <CommanModal image={imageUrl} closeModal={setOpenModal} />}
+    </>
   )
 }
 
