5c42f2ecae0b46c7529c4e4d309dfa3715a5fa25
shrey0114 <shrey@nexgsolution.com>
2022-07-28 15:02:37 +0530

    mega-menu-issue-fix

--- src/Views/StaticSite/Components/Navbar/index.jsx
+++ src/Views/StaticSite/Components/Navbar/index.jsx
@@ -14,67 +14,67 @@ const Navbar = ({ isUserLoggedIn }) => {
   const dispatch = useDispatch()
 
   return (
-    <div id='hero-overlay' >
-      <div className="navbar-container">
+    <>
+      <div id='hero-overlay' >
+        <div className="navbar-container">
+          <div
+            className="hamburger"
+            onClick={() => {
+              setNav(true)
+            }}
+          >
+          
+            {Hamburger}
+          </div>
+          <div className="title-logo">
+            <div className="header-logo">
+              <img
+                style={{ transition: 'none !important' }}
+                placeholder="none"
+                alt='The Yoga Institute'
+                src="http://ecom-static-site.oss-ap-south-1.aliyuncs.com/Home/tyi.png"
+              />
+            </div>
+            <div className="header-title"></div>
+          </div>
+          <div className="quick-actions">
+            <ul>
+              {/* <li>{Search}</li> */}
+              <Link className='comingSoon' to="/">
+                <li>{Cart}</li>
+              </Link>
+              <Link className='comingSoon' to="/">
+                <li>{Gift}</li>
+              </Link>
+              <Link onMouseOver={()=>{setDropdown(true)}} onMouseOut={()=>{setDropdown(false)}} to={isUserLoggedIn ? '/user/profile' : '/user/sign-in'}>
+                <li>{User}
+                  <div style={dropdown===true && isUserLoggedIn ?{ display:'block' }:{}} className='user-dropdown'>
+                    <ul>
+                      <li onClick={()=>navigate('/user/profile')} >User Profile</li>
+                      <li onClick={async()=>{await dispatch(logoutUserAction());navigate('/user/sign-in')}} >Logout</li>
+                    </ul>
+                  </div>
+                </li>
+              </Link>
+            </ul>
+          </div>
+        </div>
+      </div>
+      {nav && (
         <div
-          className="hamburger"
-          onClick={() => {
-            setNav(true)
+          style={{
+            position: 'fixed',
+            top: '0',
+            left: '0',
+            width: '100vw',
+            height: '100vh',
+            zIndex: '200',
           }}
         >
-          
-          {Hamburger}
+          <MegaMenu setNav={setNav} />
         </div>
-        <div className="title-logo">
-          <div className="header-logo">
-            <img
-              style={{ transition: 'none !important' }}
-              placeholder="none"
-              alt='The Yoga Institute'
-              src="http://ecom-static-site.oss-ap-south-1.aliyuncs.com/Home/tyi.png"
-            />
-          </div>
-          <div className="header-title"></div>
-        </div>
-        <div className="quick-actions">
-          <ul>
-            {/* <li>{Search}</li> */}
-            <Link className='comingSoon' to="/">
-              <li>{Cart}</li>
-            </Link>
-            <Link className='comingSoon' to="/">
-              <li>{Gift}</li>
-            </Link>
-            <Link onMouseOver={()=>{setDropdown(true)}} onMouseOut={()=>{setDropdown(false)}} to={isUserLoggedIn ? '/user/profile' : '/user/sign-in'}>
-              <li>{User}
-                <div style={dropdown===true && isUserLoggedIn ?{ display:'block' }:{}} className='user-dropdown'>
-                  <ul>
-                    <li onClick={()=>navigate('/user/profile')} >User Profile</li>
-                    <li onClick={async()=>{await dispatch(logoutUserAction());navigate('/user/sign-in')}} >Logout</li>
-                  </ul>
-                </div>
-              </li>
-            </Link>
-          </ul>
-        </div>
-        <>
-          {nav && (
-            <div
-              style={{
-                position: 'fixed',
-                top: '0',
-                left: '0',
-                width: '100vw',
-                height: '100vh',
-                zIndex: '200',
-              }}
-            >
-              <MegaMenu setNav={setNav} />
-            </div>
-          )}
-        </>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
 
