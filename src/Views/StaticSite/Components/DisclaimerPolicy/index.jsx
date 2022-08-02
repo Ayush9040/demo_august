4ddf75b23f7876b3bddcfa14e1b9057e7b5bd5ca
--global <--global>
2022-08-02 11:49:42 +0530

    new-release

--- src/Views/StaticSite/Components/DisclaimerPolicy/index.jsx
+++ src/Views/StaticSite/Components/DisclaimerPolicy/index.jsx
@@ -161,7 +161,7 @@ const DisclaimerPolicy = ({
             if(!paymentOrderResponse?.data?.amount && !paymentOrderResponse?.data?.id) return 0
             
             const options = {
-              key: 'rzp_test_udmmUPuH3rTJe8', // Enter the Key ID generated from the Dashboard
+              key: 'rzp_live_KyhtrIyJ546bd2', // Enter the Key ID generated from the Dashboard
               amount: paymentOrderResponse.data.amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
               currency: 'INR',
               name: 'The Yoga Institute',
