a4ccc2c0d28ed5e5d4399c65a63b6a774b669d14
shrey0114 <shrey@nexgeniots.com>
2022-09-22 15:54:11 +0530

    ui-updates

--- src/Views/StaticSite/Views/SearchModal/index.jsx
+++ src/Views/StaticSite/Views/SearchModal/index.jsx
@@ -1,37 +1,141 @@
 import axios from 'axios'
-import React,{ useState } from 'react'
-import { Search } from '../../assets/icons/icon'
-import BlogCard from '../../Components/BlogSection/BlogCard'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { cross, Search } from '../../assets/icons/icon'
 import './style.scss'
 
 const SearchModal = () => {
-
-  const [search,setSearch] = useState('')
-  const [ content, setContent ] = useState([])
-
+  const [search, setSearch] = useState('')
+  const [content, setContent] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   console.log(content)
 
-  const searchContent = ( )=>{
-    axios.get(`https://cms-dev-be.theyogainstituteonline.org/v1/misc/search/${ search }`).then(res=>setContent(res.data.data))
+  const searchContent = () => {
+    if( search==='' ) return
+    try {
+      setIsLoading(true)
+      axios
+        .get(
+          `https://cms-dev-be.theyogainstituteonline.org/v1/misc/search/${search}`
+        )
+        .then((res) => setContent(res.data.data))
+        .then(()=>{setIsLoading(false)})
+    } catch (err) {
+      console.log(err)
+    }
   }
 
+  console.log(isLoading, 'sdfdsa')
+
   return (
-    <div className='search-modal' >
-      <div className='close-search' >X</div>
-      <label className='search-bar' >
-        <input type='text' placeholder='Search' name='search' value={search} onChange={(e)=>{ setSearch(e.target.value)}} />
-        <span className='search-btn' onClick={searchContent} > { Search } </span>
+    <div className='search-modal'>
+      <Link to='/' >
+        <div className='close-search' >{ cross }</div>
+      </Link>
+      <h2 style={{ fontWeight:'bold',textAlign:'center' }} >Search</h2>
+      <label className='search-bar'>
+        <input
+          type='text'
+          placeholder='Search'
+          name='search'
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value)
+          }}
+        />
+        <span className='search-btn' onClick={searchContent}>
+          {Search}
+        </span>
       </label>
 
-      <div className='search-results' >
-        <h2>Blogs</h2>
-        {
-          content.posts && [content?.posts].map( item=><>
-            <BlogCard blogs={item}  />
-          </> )
-        }
-      </div>
+      {isLoading ? (
+        <div className='search-loader'><div className='loader' >Fetching Results....</div><div>Fetching Results....</div></div>
+      ) : (
+        <div className='search-results'>
+          { content?.posts && <div className='blog-results'>
+            <h3>Blogs</h3>
+            <div className='result-blogs' >
+              {content.posts &&
+              content?.posts.map((item) => (
+                <>
+                  <div className='result-item'>
+                    <div className='result-img'>
+                      <img src={item?.coverImage} />
+                    </div>
+                    <div className='result-content'>
+                      <h4
+                        style={{ fontWeight: 'bold' }}
+                        dangerouslySetInnerHTML={{ __html: `${item.title}` }}
+                      ></h4>
+                      <p
+                        dangerouslySetInnerHTML={{ __html: `${item.excerpt}` }}
+                      ></p>
+                      <Link to={`/${item?.slug}`}>
+                        <button>Read More</button>
+                      </Link>
+                    </div>
+                  </div>
+                </>
+              ))}
+            </div>
+          </div>}
+          { content?.courses && content?.courses?.length!==0 && <div className='blog-results'>
+            <h3>Courses</h3>
+            <div className='course-results' >
+              {content.posts &&
+              content?.posts.map((item) => (
+                <>
+                  <div className='result-item'>
+                    <div className='result-img'>
+                      <img src={item?.coverImage} />
+                    </div>
+                    <div className='result-content'>
+                      <h4
+                        style={{ fontWeight: 'bold' }}
+                        dangerouslySetInnerHTML={{ __html: `${item.title}` }}
+                      ></h4>
+                      <p
+                        dangerouslySetInnerHTML={{ __html: `${item.excerpt}` }}
+                      ></p>
+                      <Link to={`/${item?.slug}`}>
+                        <button>Read More</button>
+                      </Link>
+                    </div>
+                  </div>
+                </>
+              ))}
+            </div>
+          </div>}
+          { content?.pages && content?.pages?.length!==0 && <div className='blog-results'>
+            <h3>Pages</h3>
+            <div className='page-results' >
+              {content.posts &&
+              content?.posts.map((item) => (
+                <>
+                  <div className='result-item'>
+                    <div className='result-img'>
+                      <img src={item?.coverImage} />
+                    </div>
+                    <div className='result-content'>
+                      <h4
+                        style={{ fontWeight: 'bold' }}
+                        dangerouslySetInnerHTML={{ __html: `${item.title}` }}
+                      ></h4>
+                      <p
+                        dangerouslySetInnerHTML={{ __html: `${item.excerpt}` }}
+                      ></p>
+                      <Link to={`/${item?.slug}`}>
+                        <button>Read More</button>
+                      </Link>
+                    </div>
+                  </div>
+                </>
+              ))}
+            </div>
+          </div>}
+        </div>
+      )}
     </div>
   )
 }
