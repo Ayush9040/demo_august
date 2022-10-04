dc47265b510f4fb4e80de276dd2ffe1193a7acb0
shrey0114 <shrey@nexgeniots.com>
2022-10-04 11:03:34 +0530

    update:home-newletter-october

--- src/Views/StaticSite/Components/NewsLetter/index.jsx
+++ src/Views/StaticSite/Components/NewsLetter/index.jsx
@@ -34,6 +34,13 @@ const NewsLetter = () => {
         <div className="images">
           <div className="image">
             <Link to="/yogasattva">
+              <img src={`${baseDomain}${publicationAssests.ythAssets89}`} alt='Yogasattva-october' />
+            </Link>
+
+            <p>October 2022</p>
+          </div>
+          <div className="image">
+            <Link to="/yogasattva">
               <img src={`${baseDomain}${publicationAssests.ythAssets88}`} alt='Yogasattva-september' />
             </Link>
 
@@ -45,13 +52,6 @@ const NewsLetter = () => {
             </Link>
 
             <p>August 2022</p>
-          </div>
-          <div className="image">
-            <Link to="/yogasattva">
-              <img src={`${baseDomain}${publicationAssests.ythAssets86}`} alt='Yogasattva-july'/>
-            </Link>
-
-            <p>July 2022</p>
           </div>
         </div>
         <div className="description">
