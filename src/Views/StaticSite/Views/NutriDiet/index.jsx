19ecef365dc54b3ac852501848291463ff9406a9
shrey0114 <shrey@nexgeniots.com>
2022-11-01 19:58:52 +0530

    updates

--- src/Views/StaticSite/Views/NutriDiet/index.jsx
+++ src/Views/StaticSite/Views/NutriDiet/index.jsx
@@ -184,7 +184,7 @@ const NutriDiet = () => {
             <p>
             Achieve long-term wellness through simple, cultural and holistic changes in your nutrition, eating habits and cognitive thinking.
             </p>
-            <p>Call on 9967429596 for prior consultations.</p>
+            <p>Call on <a style={{ color:'blue' }} href='tel:+919967429596' >9967429596</a> for prior consultations.</p>
           </div>
         </div>
       </div>
