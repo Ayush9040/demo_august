acd6fcafe9f67ccbf741d3db27a38b37095d3ee9
sakshigupta2897 <sakshig@nexgsolution.com>
2022-11-17 12:34:19 +0530

    landing page fixed

--- src/Views/StaticSite/Views/LandingPage/ThankYouPage/index.jsx
+++ src/Views/StaticSite/Views/LandingPage/ThankYouPage/index.jsx
@@ -2,15 +2,27 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import './style.scss'
 
-const CampaignThankYou = ({ name='user' }) => {
-  const navigate =  useNavigate()
+const CampaignThankYou = ({ name = 'user', setModal, setFormData }) => {
+  const navigate = useNavigate()
   return (
     <div className="thank_you_div">
       <div className="thank_you_text_div">
-        <div onClick={()=> navigate('/ad-campaign')} className='close_btn'>&times;</div>
-        <h1>
-          Thank You { name } for signing up for the course.
-        </h1>
+        <div
+          onClick={() => {
+            setModal(false)
+            setFormData({
+              name: '',
+              email: '',
+              contact: '',
+              city: '',
+              country: '',
+            })
+          }}
+          className="close_btn"
+        >
+          &times;
+        </div>
+        <h1>Thank You {name} for signing up for the course.</h1>
         <div className="thank_you_text">
           The Yoga Institute Team will get in touch with you shortly!
         </div>
