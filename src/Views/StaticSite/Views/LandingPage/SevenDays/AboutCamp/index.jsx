9c98fc0de43c12c16d179fb6a64c6edcd533c238
mandeepsingh7290 <mandeep@nexgsolution.com>
2022-12-28 13:01:33 +0530

    changes done

--- src/Views/StaticSite/Views/LandingPage/SevenDays/AboutCamp/index.jsx
+++ src/Views/StaticSite/Views/LandingPage/SevenDays/AboutCamp/index.jsx
@@ -95,8 +95,8 @@ const AboutCamp = () => {
                 <div className="tyi-logo_21">{landingLogo}</div>
               </Link>
               <div className='phone-info'>
-                <p>Call on<a href="tel:+919136668224">&ensp;+91-9136668224</a></p>
-                <h2>Get all your queries resolved!</h2>
+                <p>CALL ON<a href="tel:+919136668224">&ensp;91-9136668224</a></p>
+                <h2>GET ALL YOUR QUERIES RESOLVED!</h2>
               </div>
             </nav>
             <div className="img_form_21">
@@ -105,7 +105,7 @@ const AboutCamp = () => {
                 <div className="left_heading2"><span className='left_heading_bold'>7-DAYS</span> </div>
                 <div className="left_heading2"><span className='left_heading_bold'>HEALTH CAMP &</span> </div>
                 <div className="left_heading3">ACHIEVE MIND-BODY</div>
-                <div className="left_heading3">BALANCE</div>
+                <div className="left_heading3">BALANCE!</div>
               </div>
               <div className="form-phone-info">
                 <form>
@@ -196,7 +196,7 @@ const AboutCamp = () => {
             />
             <div className='sec2_text_desktop'>
               <div className="sec2_text_mobile">
-                <div className="heading_21">About the Camp</div>
+                <div className="heading_21">About the Camp:</div>
                 <div className="text_21">
               The 7-Day Health Camp is an introductory course to the theoretical and practical aspects of
               Yoga. Through this camp, the participants will get to learn about the principles of Yoga, and
@@ -245,7 +245,7 @@ const AboutCamp = () => {
           <div className='registration-dates'>
             <div><span className='registration-heading'>Duration:</span></div>
             <div><span className='dates-reg'>24 th December to 30 th December, 2022</span></div>
-            <div className='dates-reg'>(Residential &amp; Non-Residential)</div>
+            <div className='dates-reg-2'>(Residential &amp; Non-Residential)</div>
             <div className='reg-note'>Please note Limited seats are available. Registrations are on a first come first serve basis.</div>
             <div><span className='registration-heading'>Timings:</span></div>
             <div className='dates-reg'>7 am to 7 pm (IST)</div>
@@ -253,8 +253,8 @@ const AboutCamp = () => {
         </section>
         <section>
           <div className='background-img' style={{ position:'relative' }} >
-            <div className="overlap-heading">OTHER POPULAR CAMPS AND WORKSHOPS</div>
             <img className='img-bg-sec' src='https://ecom-static-site.oss-ap-south-1.aliyuncs.com/seven-days/Rectangle-6-copy-5.jpg' />
+            <div className="overlap-heading">OTHER POPULAR CAMPS AND WORKSHOPS</div>
           </div>
         </section>
         <div className="popular_div21">
