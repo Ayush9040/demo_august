42366a3c024fb2dbd1d32673eeeed2e90f041f5a
sakshigupta2897 <sakshig@nexgeniots.com>
2023-03-02 12:05:05 +0530

    h1 and h2 tag issue fixed in blogs for SEO

--- src/Views/StaticSite/Views/Blogs/Views/Blog.jsx
+++ src/Views/StaticSite/Views/Blogs/Views/Blog.jsx
@@ -1,33 +1,23 @@
 import React, { useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
-//import CommonBannerNav from '../../../Components/CommonBannerNav'
 import './style.scss'
 import Heading from '../../../Components/Heading'
-//import BlogGallery from '../../../Components/BlogComponents/BlogGallery'
-//import { allBlogData } from '../../../utils/blogData'
-// import BlogParagraph from '../../../Components/BlogComponents/BlogParagraph'
-// import BlogPoints from '../../../Components/BlogComponents/BlogPoints'
-// import BlogBullets from '../../../Components/BlogComponents/BlogBullets'
-// import BlogSubHeaing from '../../../Components/BlogComponents/BlogSubHeading'
-// import BlogVideo from '../../../Components/BlogComponents/BlogVideo'
-// import BlogLink from '../../../Components/BlogComponents/BlogLink'
 import InnerNavComponent from '../../../Components/InnerNavComponent'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchBlogData } from '../Blogs.action'
 import { Helmet } from 'react-helmet'
+import { useCallback } from 'react'
 
 
 const BlogAnother = () => {
+  let text =''
 
   const dispatch = useDispatch()
 
   const { contentId } = useParams()
-  //const [pageData, setPageData] = useState({})
-  
   const { blog } = useSelector(state=>state.blogs)
 
-
-  const blogParseAlgo = (data='', title) => {
+  const blogParseAlgo = useCallback((data='', title) => {
     let headers = {
       title: '',
       links: [],
@@ -59,53 +49,16 @@ const BlogAnother = () => {
         headers.script = el
               
     })
-
+    text = headers?.title.trim()
     return <Helmet title={`${headers?.title.trim()}`} meta={ headers?.metaData } />
-  }
+    
+  },[blog?.meta, blog?.title ])
 
   useEffect(() => {
     dispatch(fetchBlogData(contentId))
     window.scrollTo(0,0)
   },[ document.title ])
 
-  // useEffect(()=>{
-  //   setPageData(blogs.find((item) => blogId === item.slug))
-  // },[blogs])
- 
-  
-
-
-
-
-  // const getComponents = (componentObj) => {
-  //   switch (componentObj.component) {
-  //   case 'blogParagraph':
-  //     return <BlogParagraph pText={componentObj.pText} />
-  //   case 'points':
-  //     return <BlogPoints points={componentObj.points} />
-  //   case 'bullets':
-  //     return (
-  //       <BlogBullets
-  //         title={componentObj.listTitle}
-  //         bullets={componentObj.bulletPoints}
-  //       />
-  //     )
-  //   case 'subHeading':
-  //     return <BlogSubHeaing boldText={componentObj.boldText} />
-  //   case 'blogVideo':
-  //     return (
-  //       <BlogVideo
-  //         width={componentObj.width}
-  //         height={componentObj.height}
-  //         url={componentObj.url}
-  //       />
-  //     )
-  //   case 'blogLink':
-  //     return <BlogLink url={componentObj.url} text={componentObj.text} />
-  //   default:
-  //     return null
-  //   }
-  // }
   const viewBlog = {
     title: 'Blogs',
     color: 'white',
@@ -120,41 +73,20 @@ const BlogAnother = () => {
         
         <InnerNavComponent abc={viewBlog}/>
         <Heading largeText={'Blog'} />
-        {/* <div className='blog-series'>
-          <div className='alumni-content' id='seminar'>
-            <div className='newsletter-content'>
-              <h2>
-                <span className='newsletter-title'>{pageData?.title}</span>
-                <span className='newsletter-date'>{pageData?.date}</span>
-              </h2>
-              <p>{pageData?.metaDescription}</p>
-            </div>
-            <div className='newsletter-image'>
-              <img
-                className='blog-iamge'
-                alt={pageData.title}
-                src={pageData?.image}
-              />
-            </div>
-          </div>
-        </div> */}
+        
       </div>
       
       
     
       { Object.keys(blog).length===0 ? <div className='global-loader' >Loading...</div> : <div className='blog-container' id='blog-container' >
-        <h1 className='meta-heading' dangerouslySetInnerHTML={{ __html:`${blog?.title}` }} ></h1>
-        <h2 dangerouslySetInnerHTML={{ __html:`${blog?.title}` }} ></h2>
+        <h1  dangerouslySetInnerHTML={{ __html:`${blog?.title}` }} ></h1>
+        <h2 className='meta-heading'> {text} </h2>
         <br/>
         { blog?.coverImage &&  <div className='blog_img' >
           <img src={`${blog?.coverImage}`} alt={`${blog?.altTag}`} />
         </div> }
         <div className='blog-grid' dangerouslySetInnerHTML={{ __html:`${blog?.content}` }} >
-          {/* <div className='blog-text'>
-            {pageData?.body?.map((comp) => {
-              return getComponents(comp)
-            })}
-          </div> */}
+         
         </div>
         <div className='tagsInput' >
           <p style={{ display:'inline-block' }} ><b >Tags:&ensp;</b></p>
