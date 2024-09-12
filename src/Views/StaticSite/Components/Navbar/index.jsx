4c44be159a19467b9612bcdf7e5e8976b7c030fd
Ayush <ayush@dotcod.in>
2024-09-12 16:08:25 +0530

    sidebar homepage overflow fixed

--- src/Views/StaticSite/Components/Navbar/index.jsx
+++ src/Views/StaticSite/Components/Navbar/index.jsx
@@ -128,7 +128,7 @@ const Navbar = ({ isUserLoggedIn, abc }) => {
             width: '100vw',
             height: '100vh',
             zIndex: '200',
-            overflow: 'scroll'
+            overflow: 'auto'
           }}
         >
           <MegaMenu setNav={setNav} />
