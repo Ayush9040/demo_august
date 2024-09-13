da8745fb8e035d54b22505ac031d06cb3ac2314a
Dinesh Pammi <dinesh.pammi@dotcod.in>
2024-09-13 15:27:31 +0530

    Cookie

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { Suspense } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import {
   Routes,
   Route,
@@ -13,9 +13,26 @@ import ReactGA from 'react-ga'
 
 
 const App = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  useEffect(() => {
+    const hasAcceptedCookies = localStorage.getItem('cookiesAccepted');
+    if (hasAcceptedCookies) {
+      setIsVisible(false);
+      ReactGA.initialize('374034779')
+      ReactGA.pageview('/')
+    }
+  }, []);
 
-  ReactGA.initialize('374034779')
-  ReactGA.pageview('/')
+  const handleAccept = () => {
+    // sessionStorage.setItem('cookiesAccepted', 'true');
+    localStorage.setItem('cookiesAccepted', 'true');
+    setIsVisible(false);
+    ReactGA.initialize('374034779')
+    ReactGA.pageview('/')
+  };
+  const handleClose = () => {
+    setIsVisible(false);
+  };
 
   return (
     <Suspense fallback={<div className='global-loader' >Loading...</div>} >
@@ -26,6 +43,21 @@ const App = () => {
         })}
         <Route path='*' element={<Pagenotfound />} />
       </Routes>
+
+      {/* badge to accept cookies */}
+      {isVisible && <div className="cookie-banner">
+        <p>
+          We use cookies to enhance your experience. By continuing to visit this site, you agree to our use of cookies.
+        </p>
+        <div>
+          <button onClick={handleClose} className="cookie-button cancel-cookie">
+            Close
+          </button>
+          <button onClick={handleAccept} className="cookie-button">
+            Accept
+          </button></div>
+      </div>}
+
     </Suspense>
   )
 }
