383b162a5d8378a6c6a2374093f9c5dcbd08ddfb
Dinesh Pammi <dinesh.pammi@dotcod.in>
2024-11-19 17:13:39 +0530

    register btn fix while updating dates

--- src/Views/StaticSite/Components/EnrollmentForm/EnrollmentForm.jsx
+++ src/Views/StaticSite/Components/EnrollmentForm/EnrollmentForm.jsx
@@ -118,7 +118,7 @@ const EnrollmentForm = ({
 
   const [selectDateValue, setSelectDateValue] = useState(values.selectDate);
   const [setDate, setSetDate] = useState(false)
-  const isSatsangPage = location.pathname === '/enrollment/satsang' || location.pathname === '/enrollment/samattvam';
+  const isSatsangPage = location.pathname?.includes("/enrollment/satsang") || location.pathname?.includes("/enrollment/samattvam");
 
   const nameFromRedux = useSelector((state) => state.auth.user.data?.firstName);
   const lastnameFromRedux = useSelector((state) => state.auth.user.data?.lastName);
